aa6c3c4a44b3b037ba15655b963c75740db685fa
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-08 01:50:11 +0530

    Added the scripts for the sorting the wealthiest person accordingly...

--- script.js
+++ script.js
@@ -53,5 +53,11 @@ function doubleMoney() {
   updateDOM();
 }
 
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
+
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
